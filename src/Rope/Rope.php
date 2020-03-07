4754ed1b3e10f5f458523d81df31bb47a59ad446
Waldemar Graban <waldekgraban@gmail.com>
2020-03-07 15:29:16 +0100

    psr fix for Rope class

--- src/Rope/Rope.php
+++ src/Rope/Rope.php
@@ -9,29 +9,29 @@ namespace Waldekgraban\StatcRopeLoad\Rope;
 
 class Rope
 {
-    protected $original_rope_length;
-    protected $rope_elasticity_constant;
-    protected $rope_cross_section;
+    protected $originalRopeLength;
+    protected $ropeElasticityConstant;
+    protected $ropeCrossSection;
 
-    public function __construct($original_rope_length, $rope_elasticity_constant, $rope_cross_section)
+    public function __construct($originalRopeLength, $ropeElasticityConstant, $ropeCrossSection)
     {
-        $this->original_rope_length     = $original_rope_length;
-        $this->rope_elasticity_constant = $rope_elasticity_constant;
-        $this->rope_cross_section       = $rope_cross_section;
+        $this->originalRopeLength     = $originalRopeLength;
+        $this->ropeElasticityConstant = $ropeElasticityConstant;
+        $this->ropeCrossSection       = $ropeCrossSection;
     }
 
     public function getOriginalRopeLength()
     {
-        return $this->original_rope_length;
+        return $this->originalRopeLength;
     }
 
     public function getRopeElasticityConstant()
     {
-        return $this->rope_elasticity_constant;
+        return $this->ropeElasticityConstant;
     }
 
     public function getRopeCrossSection()
     {
-        return $this->rope_cross_section;
+        return $this->ropeCrossSection;
     }
 }
